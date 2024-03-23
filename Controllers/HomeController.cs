5e41643dbb26560315364f192ccb44289b600953
DQuan97 <ngduyquan97@gmail.com>
2024-03-23 23:37:22 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ToDoList.Models;
+using ToDoList.Models.ToDoTask;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public static List<ToDoTaskViewModel> toDoTaskViewModels = new List<ToDoTaskViewModel>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var items = toDoTaskViewModels.ToList();
+            return View(items);
+        }
+
+        public IActionResult AddTask(string taskName, string taskStart, string taskEnd, string taskStatus, string taskDesc)
+        {
+            ToDoTaskViewModel task= new ToDoTaskViewModel 
+            {
+                Id = Guid.NewGuid(),
+                Name = taskName,
+                StartDay = taskStart,
+                EndDay = taskEnd,
+                Status = taskStatus,
+                Description = taskDesc
+            };
+            toDoTaskViewModels.Add(task);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ToDoTask/ToDoTaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Models.ToDoTask
+{
+    public class ToDoTaskViewModel
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? StartDay { get; set; }
+        public string? EndDay { get; set; }
+        public string? Status { get; set; }
+        public string? Description { get; set; }
+    }
+}
